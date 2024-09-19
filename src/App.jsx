b692fa5e12a02b88910612bf7c8330712fb092da
August <141055008+AugustIsmailov@users.noreply.github.com>
2024-09-19 16:34:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './pages/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="App">lololo</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='Header_block'>
+        <span>август, 25</span>
+        <span>Никита & Елена</span>
+    </div>
+  )
+}
+
+export default Header
